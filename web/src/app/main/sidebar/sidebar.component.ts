9ede5d973db7f2f345f574ed4980d9745f9712ea
Pia <piaznidarsic1@gmail.com>
2022-12-14 22:51:47 +0100

    Implement logout

--- web/src/app/main/sidebar/sidebar.component.ts
+++ web/src/app/main/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/shared-services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  logout(): void {
+    this.auth.logout(); 
+  }
+  
 }
