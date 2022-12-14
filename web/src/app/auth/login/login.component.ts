cbd1a5c2fad47805f405668b08fd60c700b4d419
Pia <piaznidarsic1@gmail.com>
2022-12-14 22:23:53 +0100

    Implement login endpoint call

--- web/src/app/auth/login/login.component.ts
+++ web/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import { RestService } from 'src/app/shared-services/rest.service';
 
 @Component({
   selector: 'app-login',
@@ -7,19 +8,35 @@ import {Router} from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router,) { }
+  name: string = ''; 
+  password: string = '';
+
+  constructor(private router: Router, private restService: RestService
+    ) { }
 
   ngOnInit(): void {
   }
 
-  login() {
+  async login(): Promise<void> {
     console.log("Uporabnik se je prijavil"); 
+
+    if (!this.name || !this.password) {
+      console.log('Input not OK');
+      return;
+  }
+
+    try {
+      const res = await this.restService.login(this.name, this.password); 
+      console.log(res);
+    } catch (error) {
+      
+    }
 
     // TODO: check if valid credentials
 
     // const res = await this.restService.login(this.name, this.password);
     // this.auth.setToken(res.authToken);
-    this.router.navigate(['main']);
+    // this.router.navigate(['main']);
 
   }
 
